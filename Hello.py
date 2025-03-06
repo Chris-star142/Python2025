468bb96476ac38c71711beeb35cd80abec77913d
Chris Taylor <taylorchris175@gmail.com>
2025-03-06 20:20:28 +1300

    Initial Commit

---
+++ Hello.py
@@ -0,0 +1,41 @@
+# Clear the terminal screen
+import os
+os.system('clear')
+
+####################################
+#DATA TYPES
+#Strings
+#Numbers
+#Lists
+#Tuples
+#Dictionaries
+#Boolean
+#####################################
+
+#String
+first_name = "Chris"
+
+#Number
+age = 54
+
+#List
+#names = ["John", "Bob", "Mary"]
+#print(names[0])
+
+#Tuple
+#names = ("John", "Bob", "Mary")
+#print(names[0])
+
+#Dictionary
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"
+}
+
+#print(fav_pizza["John"])
+
+#Boolean
+name = True
+
+print(name)
